980f97c3634846408555395fee809703265ace24
AayushNagarr <92391624+AayushNagarr@users.noreply.github.com>
2023-11-19 15:51:32 +0530

    Name rendering on navbar

--- Insurance/iwas/app/components/navbar.js
+++ Insurance/iwas/app/components/navbar.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import { useSession } from 'next-auth/react';
 import Logout from '../logout';
+import {useEffect, useState} from "react"
 
 const Navbar = ({ children }) => {
   const { data: session, status } = useSession();
 
+  const [user, setUser] = useState(null);
+  useEffect(() => {
+    console.log(session);
+    if(session){
+      setUser(session.user)
+    }
+  }, [session]);
   return (
     <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', background: 'blue', padding: '10px', top: 0, left: 0, right: 0, zIndex: 1000, width: '100%' }}>
       {/* Your navbar content here */}
       <nav>
+        <h1>{user?.username}</h1>
         <h1>IWAS</h1>
       </nav>
 
